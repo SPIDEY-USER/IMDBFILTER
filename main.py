328bb988664672f828c8afdd94b2c65c935da1d9
SPIDEY-USER <98372678+SPIDEY-USER@users.noreply.github.com>
2022-01-26 13:51:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ Naruto=Client(
     bot_token="5156565463:AAHUNBZIQygwcNVeBr0NZPlyP_rWnMCGQt4",
     api_id="15316155",
     api_hash="c2340e29da60393bc3c96fa7c0870911"
-)   channels="-1001798222799",
+    channels="-1001798222799",
+)
 @Naruto.on_message(filters.command("start"))
 async def start_message(bot, message):
     await message.reply_text("Hey Bro Eth Give Way Gruop Anu ketto😅")
