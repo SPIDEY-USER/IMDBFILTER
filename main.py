c51fead43c6020fd7a629f43bb31b1cd0efa8e0d
SPIDEY-USER <98372678+SPIDEY-USER@users.noreply.github.com>
2022-01-26 12:49:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-from pyrogram import Client
-
+from pyrogram import Client, filters
 
 Naruto=Client(
     "Imdb Bot",
+    bot_token="5156565463:AAHUNBZIQygwcNVeBr0NZPlyP_rWnMCGQt4",
+    api_id="15316155",
+    api_hash="c2340e29da60393bc3c96fa7c0870911"
+)
+@Naruto.on_message(filters.command("start"))
+async def start_message(bot, message):
+    await message.reply_text("hi")
+
+
+
+Naruto.run()
