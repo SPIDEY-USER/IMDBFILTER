c703060012b1caa5dfba825016805ad5a4ea9889
SPIDEY-USER <98372678+SPIDEY-USER@users.noreply.github.com>
2022-01-27 10:09:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,6 @@ async def start_message(bot, message):
           InlineKeyboardButton ("🌹REPO MAKE PART 1🌹", url="https://youtu.be/Af055Eozk9s"),
           ],[
           InlineKeyboardButton ("🌹SOURCE CODE🌹", url="https://github.com/SPIDEY-USER/IMDBFILTER"),
-          ],[
           Inlinekeyboardbutton ("🌹ADD ME TO A CHAT GRUOP🌹", url="http://t.me/{temp.U_NAME}?startgroup=true"),
           ]]
           )
